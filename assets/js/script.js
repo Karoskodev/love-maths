26b80af3298b3ef2e7faf04287d1318bfa7be517
Karosko dev <t.karavasilev@gmail.com>
2023-04-27 15:36:39 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function  calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incerementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
